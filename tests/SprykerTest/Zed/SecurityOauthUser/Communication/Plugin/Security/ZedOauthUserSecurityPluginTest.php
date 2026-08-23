3fb47e43c270fffbd7c4ee14c08d9e8031849ee5
Olha Livitchuk <77281282+olhalivitchuk@users.noreply.github.com>
2026-08-23 20:38:12 +0200

    FRW-11837 Added PHP 8.5 Support (Merge-only branch) (#1034)
    * FRW-11837 Added PHP 8.5 Support
    
    * FRW-11837 Fixed CI
    
    * FRW-11837 Fixed CI
    
    * FRW-11837 Reverted PropelOrm

--- tests/SprykerTest/Zed/SecurityOauthUser/Communication/Plugin/Security/ZedOauthUserSecurityPluginTest.php
+++ tests/SprykerTest/Zed/SecurityOauthUser/Communication/Plugin/Security/ZedOauthUserSecurityPluginTest.php
@@ -268,7 +268,6 @@ class ZedOauthUserSecurityPluginTest extends Unit
 
         $reflection = new ReflectionClass(SecurityConfigurator::class);
         $property = $reflection->getProperty('securityConfiguration');
-        $property->setAccessible(true);
         $property->setValue(null);
     }
 }
